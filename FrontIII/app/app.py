ca3073acd67454f8acdb09570a53edb98b186c3a
Landmark-GitHub <121081045+Landmark-GitHub@users.noreply.github.com>
2026-05-07 10:30:10 +0000

    what

--- FrontIII/app/app.py
+++ FrontIII/app/app.py
@@ -1,9 +1,8 @@
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import streamlit as st
 import sys
 import os
 from src.services.data_loader import prepare_all_data
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 st.set_page_config(page_title="ระบบวิเคราะห์ความเชี่ยวชาญ", page_icon="🔍")
 
