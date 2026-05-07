a52cf5baba41fd6d45ab793608dde93e1937aaf6
Landmark-GitHub <121081045+Landmark-GitHub@users.noreply.github.com>
2026-05-07 19:22:20 +0700

    Fix ROOT_DIR path construction in data_loader.py

--- FrontIII/src/services/data_loader.py
+++ FrontIII/src/services/data_loader.py
@@ -7,7 +7,7 @@ import re
 # ROOT PATH
 # =========================================================
 ROOT_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..", "..")
+    os.path.join(os.path.dirname(__file__), "..")
 )
 
 STORAGE_PATH = os.path.join(
